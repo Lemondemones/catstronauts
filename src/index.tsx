626c2d52d2c607cfbc8658a3f553a205815c9b2f
Lemondemones <sergej.omelchenko.02@mail.ru>
2024-08-01 16:28:11 +0300

    :package: Use GraphQL Code Generator for type generation and update to use `useQuery` hook as the primary API for executing queries in React

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client'
-import GlobalStyles from './styles';
-import Pages from './pages';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import GlobalStyles from "./styles";
+import Pages from "./pages";
+import { ApolloProvider } from "@apollo/client";
 
-const root = createRoot(document.getElementById('root')!);
+import { client } from "./shared/api/appoloCli";
+
+const root = createRoot(document.getElementById("root")!);
 
 root.render(
   <React.StrictMode>
-    <GlobalStyles />
-    <Pages />
+    <ApolloProvider client={client}>
+      <GlobalStyles />
+      <Pages />
+    </ApolloProvider>
   </React.StrictMode>
 );
--- src/pages/tracks.tsx
+++ src/pages/tracks.tsx
@@ -1,12 +1,44 @@
 import React from "react";
-import { Layout } from "../components";
+import { Layout, QueryResult } from "../components";
+import TrackCard from "../containers/track-card";
+
+import { gql } from "../__generated__";
+import { useQuery } from "@apollo/client";
 
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
+
+const GET_TRACKS = gql(`
+  query GetTracks {
+    tracksForHome {
+      id
+      title
+      thumbnail
+      length
+      modulesCount
+      author {
+        id
+        name
+        photo
+      }
+    }
+  }
+`);
+
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+  const { data, loading, error } = useQuery(GET_TRACKS);
+
+  return (
+    <Layout grid>
+      <QueryResult loading={loading} error={error} data={data}>
+        {data?.tracksForHome?.map((track) => (
+          <TrackCard key={track.id} track={track} />
+        ))}
+      </QueryResult>
+    </Layout>
+  );
 };
 
 export default Tracks;
